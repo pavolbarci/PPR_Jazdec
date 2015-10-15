104a9b31a55e347ef9e45960bc42888bbe57e3b7
pavolbarci <pavol.barci@gmail.com>
2015-10-15 20:04:22 +0200

    Prvotny zaciatocny projekt

---
+++ JazdecNaSachovnici/JazdecNaSachovnici.cpp
@@ -0,0 +1,35 @@
+// ppr.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+class Coordinate {
+private:
+	int x, y;
+public:
+	void setCoordinate(int, int);
+};
+
+void Coordinate::setCoordinate(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+
+int main()
+{
+	Coordinate co;
+	co.setCoordinate(1, 2);
+	return 0;
+}
+
+int val(Coordinate c)
+{
+	if (c)
+	{
+
+		return 1;
+	}
+	return 0;
+}
